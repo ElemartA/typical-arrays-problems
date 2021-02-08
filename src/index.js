7b43a9439366fc6bf969d367f6037aa6e6fde7b6
ElemartA <info88881@gmail.com>
2021-02-08 20:08:47 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (array != null && array.length != 0) {
+    let minNumber = Math.min(...array);
+    return minNumber;
+  } else {
+    return 0;
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (array != null && array.length != 0) {
+    let maxNumber = Math.max(...array);
+    return maxNumber;
+  } else {
+    return 0;
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (array != null && array.length != 0) {
+    let sumElements = 0;
+    let average;
+    for (let i = 0; i < array.length; i++) {
+      sumElements += array[i];
+    }
+    average = sumElements / array.length;
+    return average;
+  } else {
+    return 0;
+  }
 }
